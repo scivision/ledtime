67d9735093b9fd91e34ba3f657f28ad721f0cbde
U-HST0-PC\HST0 <HST0@HST0-PC.(none)>
2014-07-30 13:37:36 +0000

    adding sample rate reading

--- RunFireReader.m
+++ RunFireReader.m
@@ -1,10 +1,9 @@
 %runs the firereader program for the example fire.dat file
 % don't forget to change the 'firefn' filename for your PC.
 
-firefn = 'e:/2014-07-29/fire2.dat';
-samplerate = 1000; %a priori
-Nbool = 2; %how many data lines were read
-firedata = firereader(firefn,samplerate,Nbool);
+firefn = 'e:/firee.dat';
+Nbool = 3; %how many data lines were read
+[firedata, fs] = firereader(firefn,samplerate,Nbool);
 
 figure(1),clf(1)
 hold('on')
@@ -12,5 +11,5 @@ plot(firedata(:,1),'b','displayname','fire')
 plot(firedata(:,2),'r','displayname','1PPS')
 xlabel('sample index')
 ylabel('fire boolean')
-title('1000 samples/sec fire (sensor is exposing for fire=1)')
+title([int2str(fs),' samples/sec fire (sensor is exposing for fire=1)'])
 legend('show')
