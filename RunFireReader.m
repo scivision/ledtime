6e9a02415e490feb0615eba53712272e3937eab9
scienceopen <scienceopen@users.noreply.github.com>
2014-07-30 18:01:47 -0400

    fixing plotting bugs

--- RunFireReader.m
+++ RunFireReader.m
@@ -8,18 +8,18 @@
 % fire is the camera "answering back" logical 1 during the sensor exposure, order 10^-6 sec. error hopefully 
 %         from ExtTrig
 clear,clc
-firefn = 'e:/2014-07-30/2014-07-30T20-28-CamSer1387.fire';
+firefn = '~/junk/2014-07-30T21-36-CamSer1387.fire';
 Nbool = 3; %how many data lines were read
 [firedata, fs] = firereader(firefn,Nbool);
 
 nt = size(firedata,1);
 %plot all together
-xl = [29500,45000];
+xl = [1,550e3];
 figure(1),clf(1)
 hold('on')
-plot(firedata(:,1),'b','displayname','fire')
 plot(firedata(:,2),'r','displayname','1PPS')
 plot(firedata(:,3),'k','displayname','ExtTrig')
+plot(firedata(:,1),'b','displayname','fire') %plot last so they'll be on top!
 set(gca,'xlim',xl)
 xlabel('sample index')
 ylabel('fire boolean')
@@ -28,18 +28,21 @@ legend('show')
 %now plot separately, on same time axis
 figure(2),clf(2)
 
-subplot(3,1,1)
-area(uint8(firedata(:,2)))
+figure(2),clf(2)
+plot(firedata(:,2))
+%area(uint8(firedata(:,2)))
 ylabel('1PPS')
 set(gca,'xlim',xl)
 
-subplot(3,1,2)
-area(uint8(firedata(:,3)))
+figure(3),clf(3)
+plot(firedata(:,3))
+%area(uint8(firedata(:,3)))
 ylabel('ExtTrig')
 set(gca,'xlim',xl)
 
-subplot(3,1,3)
-area(uint8(firedata(:,1)))
+figure(4),clf(4)
+plot(firedata(:,1))
+%area(uint8(firedata(:,1)))
 ylabel('fire')
 set(gca,'xlim',xl)
 xlabel('sample index')
--- firereader.m
+++ firereader.m
@@ -2,7 +2,8 @@ function [booldata,fs] = firereader(datfn,Nbool)
 % Michael Hirsch July 2014
 % reads "fire.dat" 8-bit binary files
 
-if logical(exist('OCTAVE_VERSION','builtin'))
+isoctave = logical(exist('OCTAVE_VERSION','builtin'));
+if isoctave
 pkg load communications
 end
 
@@ -20,14 +21,24 @@ while ~feof(fid)
     i = i+1;
 	cind = ((i-1)*fs + 1): (i*fs);
     currdata = fread(fid,fs,'uint8=>uint8',0,'l'); 
-	if isempty(currdata)
+    
+    if any(currdata>2^Nbool)
+        error('We appear to not be reading your boolean data correctly')
+    end
+    
+    if isempty(currdata)
 		continue %we hit end of file, right on perfectly
 	elseif length(currdata) ~= length(cind)
 		display(['discarded ',int2str(length(currdata)),' fire pulses at the end.'])
 		continue
-	end 
-
-	booldata(cind,1:Nbool) = logical(de2bi(currdata,Nbool,'left-msb'));
+    end
+    
+    currbool = de2bi(currdata,Nbool,2,'left-msb');
+    if isoctave %FIXME till comms toolbox patched
+        booldata(cind,1:Nbool) = logical(currbool);
+    else
+        booldata(cind,1:Nbool) = currbool;
+    end
     %firedata(cind,1) = currdata;
 end
 
