b4f8a81356ecccc7a84f98db3610e7259fccd502
scienceopen <scienceopen@users.noreply.github.com>
2014-07-30 21:02:49 +0000

    third pulse complete

--- RunFireReader.m
+++ RunFireReader.m
@@ -7,17 +7,39 @@
 %         take a picture right then. 10^-6 or less error from 1PPS
 % fire is the camera "answering back" logical 1 during the sensor exposure, order 10^-6 sec. error hopefully 
 %         from ExtTrig
-
-firefn = 'e:/fire3.fire';
+clear,clc
+firefn = 'e:/2014-07-30/2014-07-30T20-28-CamSer1387.fire';
 Nbool = 3; %how many data lines were read
 [firedata, fs] = firereader(firefn,Nbool);
 
+nt = size(firedata,1);
+%plot all together
+xl = [29500,45000];
 figure(1),clf(1)
 hold('on')
 plot(firedata(:,1),'b','displayname','fire')
 plot(firedata(:,2),'r','displayname','1PPS')
 plot(firedata(:,3),'k','displayname','ExtTrig')
+set(gca,'xlim',xl)
 xlabel('sample index')
 ylabel('fire boolean')
 title([int2str(fs),' samples/sec fire (sensor is exposing for fire=1)'])
 legend('show')
+%now plot separately, on same time axis
+figure(2),clf(2)
+
+subplot(3,1,1)
+area(uint8(firedata(:,2)))
+ylabel('1PPS')
+set(gca,'xlim',xl)
+
+subplot(3,1,2)
+area(uint8(firedata(:,3)))
+ylabel('ExtTrig')
+set(gca,'xlim',xl)
+
+subplot(3,1,3)
+area(uint8(firedata(:,1)))
+ylabel('fire')
+set(gca,'xlim',xl)
+xlabel('sample index')
