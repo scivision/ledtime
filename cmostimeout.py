10fc59168cd8f85450f71d181ba331178b7f2590
michael <scienceopen@users.noreply.github.com>
2015-12-30 21:51:12 -0500

    doc

--- cmostimeout.py
+++ cmostimeout.py
@@ -2,7 +2,15 @@
 """
 Reads Calgary sCMOS .out timing files
 
-Assumption: tElapsed is time to start of exposure.
+tk0: FPGA tick when frame was taken. In this test configuration of internal trigger,
+it basically tells you, yes, the FPGA is running and knows how to count. The FPGA
+timebase could have large error (yielding large absolute time error) and yet
+this column would be exactly the same.
+
+tk1: FPGA tick after frame was retrieved, apparently to compare with 'elapsed' column
+
+elapsed: PC clock relative time since acquisition start, when frame was retrieved vis-a-vis tk1
+
 
 Michael Hirsch
 """
@@ -18,12 +26,16 @@ fn = Path('~/Dropbox/CMOScalgary/test_clock2.out').expanduser()
 dtExpected = 1/fps
 #%% parse data
 # sep uses regex for "one or more spaces"
-data = read_csv(fn,sep='\s{1,}',skiprows=14,engine='python',header=None, usecols=(1,2,3),
+data = read_csv(fn,sep='\s{1,}',skiprows=14,skipfooter=1,engine='python',
+                header=None, usecols=(1,2,3),
                 names=['elapsed','tk1','tk0'])
 
-data = data.iloc[:-1,:] #manually dropping the blank last line of the file
 N=data.shape[0]
 #%% per frame error
+
+dtick = data['tk0'].diff()
+print(dtick.unique())
+
 dt = data['elapsed'].diff()
 print(dt.describe())
 
