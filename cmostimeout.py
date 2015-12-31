637b75913ac8a095ad8db5f211474162194d5068
michael <scienceopen@users.noreply.github.com>
2015-12-30 22:02:10 -0500

    tick hist

--- cmostimeout.py
+++ cmostimeout.py
@@ -17,13 +17,14 @@ Michael Hirsch
 from numpy import arange
 from pathlib import Path
 from pandas import read_csv
-from matplotlib.pyplot import figure
+from matplotlib.pyplot import figure,subplots
 import seaborn as sns
 sns.set_context('talk',font_scale=1.5)
 #%% user parameters
 fps = 20
 fn = Path('~/Dropbox/CMOScalgary/test_clock2.out').expanduser()
 dtExpected = 1/fps
+tick_sec = 1/40e6 # we suppose the FPGA clock cycle is 40MHz. tick_sec is the period of the tick, assuming zero timebase error (real life timebase has substantial error)
 #%% parse data
 # sep uses regex for "one or more spaces"
 data = read_csv(fn,sep='\s{1,}',skiprows=14,skipfooter=1,engine='python',
@@ -33,18 +34,29 @@ data = read_csv(fn,sep='\s{1,}',skiprows=14,skipfooter=1,engine='python',
 N=data.shape[0]
 #%% per frame error
 
-dtick = data['tk0'].diff()
-print(dtick.unique())
+dtick = data['tk1'].diff()
+dtick_sec = dtick*tick_sec
+print(dtick_sec.describe())
 
 dt = data['elapsed'].diff()
 print(dt.describe())
 
-ax = figure().gca()
+fg,axs = subplots(1,2)
+
+ax = axs[0]
+ax.set_title('PC time')
 dterr = dt - dtExpected
 dterr.hist(ax=ax,bins=100)
-ax.set_yscale('log')
-ax.set_title('Per-frame timing error, N={}  fps={}'.format(N,fps))
-ax.set_xlabel('time error [sec.]')
+
+ax = axs[1]
+ax.set_title('FPGA time')
+dtickerr = dtick_sec - dtExpected
+dtickerr.hist(ax=ax,bins=100)
+
+fg.suptitle('Per-frame timing error, N={}  fps={}'.format(N,fps),size='xx-large')
+for a in axs:
+    a.set_yscale('log')
+    a.set_xlabel('time error [sec.]')
 #%% accumulated error (bias)
 expectedElapsed = arange(N) * dtExpected
 elapsedError = data['elapsed'] - expectedElapsed
